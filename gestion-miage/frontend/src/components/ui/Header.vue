79ea0e41a11a4656be87b3a8227acc2673ead6e2
UNIVERS10 <joelattitso@gmail.com>
2025-05-18 13:44:09 +0200

    Refactor Header component to use CommonHeader for improved structure and maintainability

--- gestion-miage/frontend/src/components/ui/Header.vue
+++ gestion-miage/frontend/src/components/ui/Header.vue
@@ -1,27 +1,4 @@
 <template>
-<<<<<<< HEAD:gestion-miage/frontend/src/components/ui/Header.vue
-    <header class="bg-brandBlue h-16 flex items-center pl-64 justify-center text-white shadow">
-      <div class="flex items-center">
-        <font-awesome-icon :icon="icon" class="mr-2 text-brandGreen" />
-        <h1 class="text-xl font-semibold">{{ title }}</h1>
-      </div>
-    </header>
-  </template>
-  
-  <script setup>
-  const props = defineProps({
-    title: {
-      type: String,
-      required: true
-    },
-    icon: {
-      type: Array,
-      required: true
-    }
-  })
-  </script>
-  
-=======
   <CommonHeader :title="title" :icon="icon">
     <template #actions>
       <!-- Actions spécifiques au secrétariat si nécessaire -->
@@ -37,4 +14,24 @@ defineProps({
   icon: Array
 })
 </script>
->>>>>>> auditFront:gestion-miage/frontend/src/components/secretariat/Header.vue
+    <header class="bg-brandBlue h-16 flex items-center pl-64 justify-center text-white shadow">
+      <div class="flex items-center">
+        <font-awesome-icon :icon="icon" class="mr-2 text-brandGreen" />
+        <h1 class="text-xl font-semibold">{{ title }}</h1>
+      </div>
+    </header>
+  </template>
+
+  <script setup>
+  const props = defineProps({
+    title: {
+      type: String,
+      required: true
+    },
+    icon: {
+      type: Array,
+      required: true
+    }
+  })
+  </script>
+
